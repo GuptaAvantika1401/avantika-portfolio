c3b0003e814fd74a7b3598f7d448d4e7927e0356
Avantikagupta1401 <gupta.avantika1401@gmail.com>
2025-05-25 11:08:50 +0530

    Initial commit with deployment setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { CertificationsComponent } from './components/certifications/certifications.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent,AboutComponent,SkillsComponent,ExperienceComponent,CertificationsComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
